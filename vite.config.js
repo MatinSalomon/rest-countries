9bb4d3fb0dd1f256cd4485d70c3bf54ec37e19d5
martin salomon <salomonmartin21@gmail.com>
2026-05-01 14:59:32 -0300

    vite config modified

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {} // fuerza rollup en lugar de rolldown
+  },
+  environments: {
+    client: {
+      build: {
+        rollupOptions: {}
+      }
+    }
+  }
 })
